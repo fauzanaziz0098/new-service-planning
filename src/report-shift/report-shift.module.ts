31953d010970b16cdce0fe9172681a28e1dc52a9
rahmat-19 <rahmatjr023@gmail.com>
2023-09-07 11:19:17 +0700

    update: config report shift

--- src/report-shift/report-shift.module.ts
+++ src/report-shift/report-shift.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReportShiftController } from './report-shift.controller';
 import { ReportShiftService } from './report-shift.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReportShift } from './entities/report-shift.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ReportShift])],
   controllers: [ReportShiftController],
-  providers: [ReportShiftService]
+  providers: [ReportShiftService],
 })
 export class ReportShiftModule {}
